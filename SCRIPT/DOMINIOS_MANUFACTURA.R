4bcc2af7fc674d3698b55116af9c952930d972ff
angelomar593 <omarpbrasta@hotmail.com>
2023-08-13 21:51:35 -0500

    Actualizacion

--- SCRIPT/DOMINIOS_MANUFACTURA.R
+++ SCRIPT/DOMINIOS_MANUFACTURA.R
@@ -1,3 +1,11 @@
+
+# CARGAMOS EL DIRECTORIO 2020 ---------------------------------------------
+directorio_2020 <- readRDS("DATA/DIRECTORIO/2020/directorio_20220412.rds") %>%
+  filter(anio==2020)
+
+directorio_2020 <- directorio_2020 %>%  mutate(id_empresa=as.character(id_empresa),
+                                               dom = paste0(tamanou_plazas,codigo_seccion))
+
 a_1 <- marco_2021_canasta %>% filter(codigo_seccion=="C",tamanou_plazas!=5) %>% 
   group_by(codigo_division,codigo_actividad_eco) %>% 
   summarise(n()) 
@@ -12,7 +20,6 @@ a_1 %>% group_by(codigo_division) %>% summarise(Total_cod_2 = n()) %>%
     by="codigo_division"
     ) %>% View()
 
-
 # CALCULO DE TAMAÑO -------------------------------------------------------
 nc=0.9
 z=qnorm(nc+(1-nc)/2)
@@ -20,7 +27,7 @@ er=0.1
 base <- marco_2021_canasta %>% filter(codigo_seccion=="C",tamanou_plazas!=5)
 
 tam_c <- base %>% 
-  mutate(dominio=codigo_division,
+  mutate(dominio=paste0(tamanou_plazas,codigo_division),
          ventas_totales=as.numeric(ventas_totales)) %>% 
   group_by(dominio) %>% 
   summarise(N=n(),
@@ -30,9 +37,9 @@ tam_c <- base %>%
          denominador=((N-1)/N)*((er*ventas/z)^2)+N*(desv^2),
          tam=numerador/denominador) %>% 
   left_join(select(tnr,dominio,tnr_max,tnr_pro, tnr_min),by="dominio") %>% 
-  mutate(tnr_max=ifelse(is.na(tnr_max),0,tnr_max/100),
-         tnr_pro=ifelse(is.na(tnr_pro),0,tnr_pro/100),
-         tnr_min=ifelse(is.na(tnr_min),0,tnr_min/100),
+  mutate(tnr_max=0.2,#ifelse(is.na(tnr_max),0,tnr_max/100),
+         tnr_pro=0.2,#ifelse(is.na(tnr_pro),0,tnr_pro/100),
+         tnr_min=0.2,#ifelse(is.na(tnr_min),0,tnr_min/100),
          n1=ceiling(tam/(1-tnr_max)),
          n2=ifelse(n1>N,N,n1),
          n3=ceiling(tam/(1-tnr_pro)),
@@ -42,8 +49,22 @@ tam_c <- base %>%
 
 tam_c %>% select(dominio,N,n4) %>% 
   mutate(Porc = n4*100/N) %>% 
-  adorn_totals()  %>% View()
+  adorn_totals()  %>% View("TAMAÑO")
 
+# GRAFICOS ----------------------------------------------------------------
 
+grafica <- marco_2021_canasta %>% filter(codigo_seccion=="C",tamanou_plazas!=5,
+                                         codigo_division=="C10")
 
+ggplot(grafica %>% group_by(tamanou_plazas) %>% summarise(Total=sum(ventas_totales,na.rm = T)),
+       aes(Total,fill=as.character(tamanou_plazas)))+geom_bar(position="dodge")
 
+ggplot(grafica,
+       aes(codigo_division,fill=as.character(tamanou_plazas)))+geom_bar(position="dodge")
+
+# DESVIACIONES POR DOMINIO ------------------------------------------------
+
+ggplot(grafica,
+       aes(codigo_division,fill=as.character(tamanou_plazas)))+geom_bar(position="dodge")
+
+ggplot(tam_c,aes(y=desv,x=dominio))+geom_point()
