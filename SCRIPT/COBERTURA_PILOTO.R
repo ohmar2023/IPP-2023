b1a178ffb594df8120e6d4974399eb97ccc1cde1
ohmar2023 <inec.2023.omar@gmail.com>
2024-03-11 17:01:25 -0500

    Piloto 002

--- SCRIPT/COBERTURA_PILOTO.R
+++ SCRIPT/COBERTURA_PILOTO.R
@@ -1,8 +1,10 @@
 library(readxl)
+library(janitor)
+library(ggplot2)
 
-
+#--------------------------------------------------------------------------
 # LECTURA DE BASES DE DATOS -----------------------------------------------
-
+#--------------------------------------------------------------------------
 
 data_muestra_f <- read_excel("PILOTO COBERTURA/Muestra_Empresas_CAB-SIPP_v1.xlsx", 
                                            sheet = "Consolidado Forzosa") %>% clean_names()
@@ -11,7 +13,9 @@ data_muestra_m_p <- read_excel("PILOTO COBERTURA/Muestra_Empresas_CAB-SIPP_v1.xl
                              sheet = "Consolidado Pequeñas Medianas") %>% clean_names()
 
 
+#--------------------------------------------------------------------------
 # GRANDES -----------------------------------------------------------------
+#--------------------------------------------------------------------------
 
 DB1 <- data_muestra_f %>% select(id_empresa,ciiu_letra,codigo_actividad_eco,
                                  paste0("codigo_producto_",c(1:5))) %>% 
@@ -20,17 +24,34 @@ DB1 <- data_muestra_f %>% select(id_empresa,ciiu_letra,codigo_actividad_eco,
     names_to = "Producto",
     values_to = "Codigo")
 
-DB1 %>% group_by(Codigo) %>% summarise(n()) %>% adorn_totals() %>% View("DB1")
+resumen_1 <- DB1 %>% group_by(Codigo) %>% summarise(n_g=n()) 
 
-
+#--------------------------------------------------------------------------
 # MEDIANAS PEQUEÑAS -------------------------------------------------------
-
+#--------------------------------------------------------------------------
 
 DB2 <- data_muestra_m_p %>% select(id_empresa,ciiu_1,codigo_actividad_eco,
                                  paste0("codigo_producto_",c(1:5))) %>% 
   pivot_longer(
     cols = c(paste0("codigo_producto_",c(1:5))),
     names_to = "Producto",
-    values_to = "Codigo")
+    values_to = "Codigo") %>% 
+  mutate(Codigo = gsub("\\.","",as.character(Codigo)))
 
-DB2 %>% group_by(Codigo) %>% summarise(n()) %>% adorn_totals() %>% View("DB2")
+resumen_2 <- DB2 %>% group_by(Codigo) %>% summarise(n_mp=n()) 
+
+#--------------------------------------------------------------------------
+# GRAFICO -----------------------------------------------------------------
+#--------------------------------------------------------------------------
+
+aux <- resumen_1 %>% full_join(resumen_2,by="Codigo") %>% 
+  adorn_totals(c("col"))
+
+aux %>% filter(!is.na(Codigo),Total>=5) %>%
+  ggplot(aes(x=Codigo,y=Total,fill = Codigo)) + 
+  geom_bar(stat="identity")+
+  theme(legend.position = "bottom")
+
+
+
+  
